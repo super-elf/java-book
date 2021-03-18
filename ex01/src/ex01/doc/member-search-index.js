81b64479353d6640252c9be637b225fe8440ced6
super-elf <71476335+super-elf@users.noreply.github.com>
2021-03-18 14:40:12 +0800

    Add files via upload

---
+++ ex01/src/ex01/doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"ex01","c":"Hi","l":"Hi()","url":"%3Cinit%3E()"},{"p":"ex01","c":"Hi","l":"main(String[])","url":"main(java.lang.String[])"}]
---
+++ ex01/src/ex01/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ex01","l":"Hi"}]
